275c25c812a52f837043f76bee73f74d0142c75e
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-08-04 23:48:23 +0530

    Leaderboard Optimisation

--- controllers/premiumFeatures.js
+++ controllers/premiumFeatures.js
@@ -6,23 +6,19 @@ const sequelize = require('../utils/database');
 
 exports.getUserLeaderboard = async (req,res)=> {
     try {
-      const users = await User.findAll()
-      const expenses = await Expense.findAll();
-      const userAggregateExpenses = {}
-      expenses.forEach((expense)=> {
-        if(userAggregateExpenses[expense.userId]){
-            userAggregateExpenses[expense.userId]=userAggregateExpenses[expense.userId]+expense.expense
-        }
-        else{
-            userAggregateExpenses[expense.userId]=expense.expense
-        }
+      const leaderboardofUser = await User.findAll({
+        attributes: ['id','name',[sequelize.fn('sum',sequelize.col('expenses.expense')),'total_Expense']],
+        include: [
+            {
+                model: Expense,
+                attributes:[]
+            }
+        ],
+        group:['user.id'],
+        order:[['total_Expense','DESC']]
       })
-      var userLeaderboardDetails=[];
-      users.forEach((user)=> {
-        userLeaderboardDetails.push({name: user.name, total_Expense: userAggregateExpenses[user.id]||0})
-      })
-      userLeaderboardDetails.sort((a,b)=> b.total_Expense-a.total_Expense)
-      res.status(200).json(userLeaderboardDetails);
+      
+      res.status(200).json(leaderboardofUser);
     }
     catch(err){
         console.log(err);
